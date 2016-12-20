2f003dee6d261d368b4f1a37cf554cdd57f2f0e6
Todor77 <toshe_ilievski@hotmail.com>
2016-12-20 10:10:36 +0100

    Commit with source control

---
+++ Recipees/Recipees/Controllers/StoreController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Recipees.Controllers
+{
+    public class StoreController : Controller
+    {
+        private recipesEntities recipesDB = new recipesEntities();
+        // GET: Store
+        public ActionResult Index()
+        {
+            var recipe = this.recipesDB.recipe_ingredient;
+
+            return this.View(recipe);
+        }
+    }
+}
