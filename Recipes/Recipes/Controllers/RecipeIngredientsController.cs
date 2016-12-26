5c3081701f491885bbf58e49793d03cc3b532fce
Todor77 <toshe_ilievski@hotmail.com>
2016-12-26 10:17:22 +0100

    Last changes

---
+++ Recipes/Recipes/Controllers/RecipeIngredientsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Recipes;
+
+namespace Recipes.Controllers
+{
+    public class RecipeIngredientsController : Controller
+    {
+        private RecipeEntities1 db = new RecipeEntities1();
+
+        // GET: RecipeIngredients
+        public async Task<ActionResult> Index()
+        {
+            var recipeIngredients = db.RecipeIngredients.Include(r => r.Ingredient).Include(r => r.Recipe);
+            return View(await recipeIngredients.ToListAsync());
+        }
+
+        // GET: RecipeIngredients/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RecipeIngredient recipeIngredient = await db.RecipeIngredients.FindAsync(id);
+            if (recipeIngredient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(recipeIngredient);
+        }
+
+        // GET: RecipeIngredients/Create
+        public ActionResult Create()
+        {
+            ViewBag.IngredientID = new SelectList(db.Ingredients, "IngredientID", "Name");
+            ViewBag.RecipeID = new SelectList(db.Recipes, "RecipeID", "Name");
+            return View();
+        }
+
+        // POST: RecipeIngredients/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,RecipeID,IngredientID")] RecipeIngredient recipeIngredient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.RecipeIngredients.Add(recipeIngredient);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IngredientID = new SelectList(db.Ingredients, "IngredientID", "Name", recipeIngredient.IngredientID);
+            ViewBag.RecipeID = new SelectList(db.Recipes, "RecipeID", "Name", recipeIngredient.RecipeID);
+            return View(recipeIngredient);
+        }
+
+        // GET: RecipeIngredients/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RecipeIngredient recipeIngredient = await db.RecipeIngredients.FindAsync(id);
+            if (recipeIngredient == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IngredientID = new SelectList(db.Ingredients, "IngredientID", "Name", recipeIngredient.IngredientID);
+            ViewBag.RecipeID = new SelectList(db.Recipes, "RecipeID", "Name", recipeIngredient.RecipeID);
+            return View(recipeIngredient);
+        }
+
+        // POST: RecipeIngredients/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,RecipeID,IngredientID")] RecipeIngredient recipeIngredient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(recipeIngredient).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IngredientID = new SelectList(db.Ingredients, "IngredientID", "Name", recipeIngredient.IngredientID);
+            ViewBag.RecipeID = new SelectList(db.Recipes, "RecipeID", "Name", recipeIngredient.RecipeID);
+            return View(recipeIngredient);
+        }
+
+        // GET: RecipeIngredients/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RecipeIngredient recipeIngredient = await db.RecipeIngredients.FindAsync(id);
+            if (recipeIngredient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(recipeIngredient);
+        }
+
+        // POST: RecipeIngredients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            RecipeIngredient recipeIngredient = await db.RecipeIngredients.FindAsync(id);
+            db.RecipeIngredients.Remove(recipeIngredient);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
